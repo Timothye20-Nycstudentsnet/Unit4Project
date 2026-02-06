6ed860158ead30f914717c7192e679c8817d3da8
Timothye20-Nycstudentsnet <timothye20@nycstudents.net>
2026-02-06 14:47:17 -0500

    Add files via upload

---
+++ HandClass.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+
+public class HandClass {
+    String [] handArray;
+    String handOccurences;
+    int bettingValue;
+
+    // So we'll refrence each of these occurences across each hand for its value
+    static int parseThroughHandResults(int i, String[] currentHand, String[] wordOfCard){
+        int parsingResults = 0; //Counter of how many results of what we're parsing for (CurrentHand)
+        String lookingFor = wordOfCard[i]; //This is the item in word order we are looking for
+        for (String currentHandValue: currentHand) { // stores each value in Currenthand as currenthandvalue
+            if (currentHandValue.equals(lookingFor)){ // if it equals item we're lookign for looking for
+                parsingResults++;
+            }
+        }
+        return parsingResults;
+    }
+
+    static String determineHandType (String determiningHand) {
+
+        //  0 0 ... 1 0 1 0
+
+        String hand; // 5oAK 4oAK FH 3oAK 2p 1p HC
+        boolean contains5;
+        boolean contains4;
+        boolean contains3;
+        boolean contains2;
+        boolean double2;
+
+
+        if (determiningHand.contains("5")) {
+            contains5 = true;
+        } else {
+            contains5 = false;
+        }
+
+        if (determiningHand.contains("4")) {
+            contains4 = true;
+        } else {
+            contains4 = false;
+        }
+
+        if (determiningHand.contains("3")) {
+            contains3 = true;
+        } else {
+            contains3 = false;
+        }
+
+        int first2Index = determiningHand.indexOf("2");
+        if (first2Index != -1) {
+            contains2 = true;
+            // Search for another "2" starting from the position after the first one
+            double2 = determiningHand.indexOf("2", first2Index + 1) != -1; // Double 2 is if the substring after the point contains another 2
+        } else {
+            contains2 = false;
+            double2 = false;
+        }
+
+
+        // labeling begins here
+
+        if (contains5) {
+            hand = "5oAK";
+        } else if (contains4) {
+            hand = "4oAK";
+        } else if (contains3 && contains2) {
+            hand = "FH";
+        } else if (contains3) {
+            hand = "3oAK";
+        } else if (double2) {
+                hand = "2p";
+        } else if (contains2) {
+            hand = "1p";
+        } else {
+            hand = "HC";
+        }
+
+        return hand;
+    }
+
+
+    public HandClass(int bettingValue, String [] handArray, String handOccurences){
+        this.bettingValue = bettingValue;
+        this.handArray = handArray;
+        this.handOccurences = handOccurences;
+
+    }
+
+    public String toString( ){
+        return bettingValue + " " + Arrays.toString(handArray);
+    }
+
+
+}
--- Main.java
+++ Main.java
@@ -20,32 +20,37 @@ public class Main {
 
 
         String[] lines = fileData.split("\n"); // splits line to line
-        int fivOfAKind = 0;
+        HandClass[] allHands = new HandClass[lines.length];
+        int fivOfAKind = 0; // Stores these for Part 1.
         int fouOfAKind = 0;
         int fullHouse = 0;
         int throfAKind = 0;
         int twoPair = 0;
         int onePair = 0;
         int highCard = 0;
+        int index = 0;
         for (String line : lines) {
+            int handClassIndex = 0; //This is so that we can organize handclassees by integer too
 
             String[] hand = line.split("\\|", 2); // Split line into Array of 2 items. the Cardnames (String) & The betting power)
 
             int handsStrength = Integer.parseInt(hand[1].trim()); //Store betting power in this int
-            String handNames = hand[0]; //String of all the Cardnames so u can split them into an array
+            String handNames = hand[0]; //String of all the Cardnames so u can split them into an array. We can store this to parse through later.
             String[] handArray = handNames.split(","); //HandArray contains an array of all the CardNames
 
             String[] wordOfCard = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-            int[] handvalues = new int[wordOfCard.length]; // Handvalues contains the OCCURENCE DATA of each Card (Ordered as in Word of Card)
-            String handvaluesString = "";
+            int[] handvaluesArray = new int[wordOfCard.length]; // Handvalues contains the OCCURENCE DATA of each Card (Ordered as in Word of Card)
+            String handvaluesString = ""; // Saving as both an array and string because sometimes we'll need item by item
 
 
             for (int i = 0; i < wordOfCard.length; i++) {
-                handvalues[i] = MethodStorage.parseThroughHandResults(i, handArray, wordOfCard);
-                handvaluesString += handvalues[i]; //If we can just get handvalues as a string we can then just parse normally
+                handvaluesArray[i] = HandClass.parseThroughHandResults(i, handArray, wordOfCard); // This assigns occurence data (zero 2's. one 3, zero 4's. two 5... = 0102...)
+                handvaluesString += handvaluesArray[i]; //If we can just get handvalues as a string we can then just parse normally
             }
 
-            String handType = MethodStorage.determineHandType(handvaluesString);
+
+            allHands[index] = new HandClass(handsStrength, handArray, handvaluesString);
+            String handType = HandClass.determineHandType(handvaluesString); // HandType defines itself as the result of determinginghandtype process. Aka its wha stores the type of ahdn it is
 
             if (handType == "5oAK") {
                 fivOfAKind++;
@@ -62,6 +67,7 @@ public class Main {
             } else {
                 highCard++;
             }
+            index++;
 
         }
         System.out.println("Number of five of a kind hands: " + fivOfAKind);
@@ -72,6 +78,11 @@ public class Main {
         System.out.println("Number of one pair hands: " + onePair);
         System.out.println("Number of high card hands: " + highCard);
 
+        // System.out.println(Arrays.toString(allHands));
+
+        for (HandClass HandClass: allHands){
+
+        }
     }
 }
 
