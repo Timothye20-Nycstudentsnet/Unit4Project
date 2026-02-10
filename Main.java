af84a79c0f54b3191edc94070f25195b6174005a
Timothye20-Nycstudentsnet <timothye20@nycstudents.net>
2026-02-10 14:50:13 -0500

    Add files via upload
    ieutgai5ehgoajih4wiuhnao4hjy 2/10 WORKCLASS

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -55,6 +56,7 @@ public class Main {
 
 
 
+
             String handType = currentHandObject.getHandType(); // HandType defines itself as the result of determinginghandtype process. Aka its wha stores the type of ahdn it is
 
             if (handType == "5oAK") {
@@ -86,18 +88,19 @@ public class Main {
         // System.out.println(Arrays.toString(allHands));
 
         for (HandClass handClassinArray: allHands){
+            String wordOfCardString = "2345678910JackQueenKingAce";
             int preliminaryRank; // The idea is Immediately rank using the variables. All the 50aks tie for first, the FH's for the spot under.. etc
             if (handClassinArray.handType == "5oAK") {
                 preliminaryRank = 1;
-            } else if (handClassinArray.handType  == "FH") {
+            } else if (handClassinArray.handType.equals("FH")) {
                 preliminaryRank = 1 + fivOfAKind;
-            } else if (handClassinArray.handType  == "4oAK") {
+            } else if (handClassinArray.handType.equals("4oAK")) {
                 preliminaryRank = 1 + fivOfAKind + fullHouse;
-            } else if (handClassinArray.handType == "3oAK") {
+            } else if (handClassinArray.handType.equals("3oAK")) {
                 preliminaryRank = 1 + fivOfAKind + fullHouse + fouOfAKind;
-            } else if (handClassinArray.handType == "2p") {
+            } else if (handClassinArray.handType.equals("2p")) {
                 preliminaryRank = 1 + fivOfAKind + fullHouse + fouOfAKind + throfAKind;
-            } else if (handClassinArray.handType  == "1p") {
+            } else if (handClassinArray.handType.equals("1p")) {
                 preliminaryRank = 1 + fivOfAKind + fullHouse + fouOfAKind + throfAKind + twoPair;
             } else {
                 preliminaryRank = 1 + fivOfAKind + fullHouse + fouOfAKind + throfAKind + twoPair + onePair;
@@ -108,6 +111,59 @@ public class Main {
             // Ok now we need to organize them based within the rank
             // My idea is to take the first object & Parse within wordofCard to compare. [Hand1.Item1.Index > hand2.Item1 Index] and the loser would increase rank by 1
             // For Each object (Go up against every other) if Handtypes are equal and it loses out then rank goes up by 1.
+
+            boolean completedMatchup = false; // turn this true to end the loop
+            int handitemindex = 0; // we'll make an index tha goes through the first 4 items for comparisons sake
+            int comparisonsRankCounter = 0; // this counts how many times it LOSES a matchup. we only add when it loses cause we only need to know how many other hands its worse than
+            for (HandClass comparisonHand: allHands){
+                if (handClassinArray == comparisonHand) {
+                    continue; // Skips same hand
+                }
+
+                System.out.println("FirstItem Hand" + Arrays.toString(handClassinArray.handArray));
+                System.out.println("Comparison Hand" + Arrays.toString(comparisonHand.handArray)); // The comparison hand is a duplicate of the first item hand!!!
+                String firstHandArrayItem = handClassinArray.handArray[handitemindex];
+                String comparisonHandArrayItem = comparisonHand.handArray[handitemindex]; // These give u the current item
+
+                int locationOfFirstHandItem = wordOfCardString.indexOf(firstHandArrayItem);
+                System.out.println("Location of item " + firstHandArrayItem + " = " + locationOfFirstHandItem);
+                int locationOfComparisonHandItem = wordOfCardString.indexOf(comparisonHandArrayItem);
+                System.out.println("Location of Comparison item " + comparisonHandArrayItem + " = " + locationOfComparisonHandItem);
+
+
+                while (completedMatchup != true) {
+                    if (!handClassinArray.handType.equals(comparisonHand.handType)) { // if theyre NOT even the same hand, end early
+                        completedMatchup = true;
+                        System.out.println("Incompatable hand type, skip");
+                    } else if (locationOfFirstHandItem > locationOfComparisonHandItem) { // If they're the same hand & The comparison item has the lesser index (appears earlier)
+                        comparisonsRankCounter ++; // Loss
+                        completedMatchup = true;
+                        System.out.println("Loss");
+                    } else if (locationOfFirstHandItem < locationOfComparisonHandItem) {
+                        completedMatchup = true; // Victory. Skip
+                        System.out.println("Victory");
+                    } else { // In this case it appears to be a tie
+                        handitemindex++; // Go to next number
+                        System.out.println("Tie. Index is now " + handitemindex);
+                        firstHandArrayItem = handClassinArray.handArray[handitemindex];
+                        comparisonHandArrayItem = comparisonHand.handArray[handitemindex]; // These give u the current item
+                        locationOfFirstHandItem = wordOfCardString.indexOf(firstHandArrayItem);
+                        locationOfComparisonHandItem = wordOfCardString.indexOf(comparisonHandArrayItem); // Redefine everything so you can recompare
+                        System.out.println("Location of item " + firstHandArrayItem + " = " + locationOfFirstHandItem);
+                        System.out.println("Location of Comparison item " + comparisonHandArrayItem + " = " + locationOfComparisonHandItem);
+
+                        // No completed matchup because they should keep going until something is decided. Each hand is unique so there WILL eventually be an item where they'll differ
+                    }
+                }
+
+                preliminaryRank += comparisonsRankCounter;
+                handClassinArray.setRank(preliminaryRank);
+                // The current item has ran through all its matches
+
+                System.out.println("Matchups Lost: " + comparisonsRankCounter + "Final Rank: " + preliminaryRank);
+
+            }
+
         }
     }
 }
